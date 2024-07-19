df054b38d3f655e85b174627242d0e4a6af1dd8e
DDDustinZ <52795360+dddustinz@users.noreply.github.com>
2024-07-19 16:50:28 -0500

    add RecordNotFound coverage on query

--- tests/IntegrationTests/Blogs/GetBlogByIdTests.cs
+++ tests/IntegrationTests/Blogs/GetBlogByIdTests.cs
@@ -1,4 +1,6 @@
-﻿using COMPANY_NAME.PRODUCT.UseCases.Blogs;
+﻿using COMPANY_NAME.PRODUCT.Core.Exceptions;
+using COMPANY_NAME.PRODUCT.UseCases.Blogs;
+using FluentAssertions;
 using IntegrationTests.Common;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,5 +18,15 @@ public class GetBlogByIdTests(IntegrationDbFixture fixture) : IntegrationDbTestB
         var actual = await _sut.Handle(request, CancellationToken.None);
 
         await Verify(actual, VerifySettingsFactory.Default);
+    }
+    
+    [Fact]
+    public async Task Handle_ReturnsNotFound_WhenNoResultsFromDb()
+    {
+        var request = new GetBlogByIdQuery(9999);
+
+        var act = async () => await _sut.Handle(request, CancellationToken.None);
+
+        await act.Should().ThrowAsync<RecordNotFoundException>();
     }
 }
